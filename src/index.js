afdd9cf7627a540da8f522c6492e7a2bca21ad8f
tuyengabi <tuyen.gabi@gmail.com>
2019-05-15 14:26:19 +0200

    declare destination for container

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App.js'
+import TodoList from './TodoList'
 
-ReactDOM.render(<App />, document.getElementById('container'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+let destination = document.querySelector('#container');
+ReactDOM.render(
+  <div>
+    <TodoList />
+  </div>,
+  destination
+);
